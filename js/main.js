c838c771f1d0b82cd373932072e3f4365ca01eac
Mojo32100 <97665404+Mojo32100@users.noreply.github.com>
2022-08-21 14:03:31 +0530

    Update main.js
    change done

--- js/main.js
+++ js/main.js
@@ -177,3 +177,36 @@ $(() => {
     return $(this).toggleClass("on");
   });
 }.call(this));
+
+
+
+
+// read-more-less
+$('.moreless-button1').click(function() {
+  $('.moretext1').slideToggle();
+  if ($('.moreless-button1').text() == "Read more") {
+    $(this).text("Read less")
+  } else {
+    $(this).text("Read more")
+  }
+});
+
+
+$('.moreless-button2').click(function() {
+  $('.moretext2').slideToggle();
+  if ($('.moreless-button2').text() == "Read more") {
+    $(this).text("Read less")
+  } else {
+    $(this).text("Read more")
+  }
+});
+
+
+$('.moreless-button3').click(function() {
+  $('.moretext3').slideToggle();
+  if ($('.moreless-button3').text() == "Read more") {
+    $(this).text("Read less")
+  } else {
+    $(this).text("Read more")
+  }
+});
